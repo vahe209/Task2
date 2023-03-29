24f7423d2c51254984608932acc48b496e979131
Vahe Hovsepyan <vahehovsepyan3@gmail.com>
2023-03-30 01:12:46 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
+public class Main  {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Your Name");
+        String a1 = sc.nextLine();
+        System.out.println("How old are you?");
+        int a2 = sc.nextInt();
+        boolean isString = false;
+        try {
+            int i = parseInt(a1);
+        } catch (NumberFormatException ex) {
+            isString = true;
+        }
+
+
+        Student student1 = new Student(a1, a2);
+        if (isString) {
+            System.out.println("My name is " + student1.getName() + ", I'm " + student1.getYear());
+        } else {
+           throw new InputMismatchException("In name field you can't write numbers");
+        }
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String name;
+    private int year;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Student(String name, int year) {
+this.name = name;
+this.year = year;
+    }
+}
